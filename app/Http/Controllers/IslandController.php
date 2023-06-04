37c9bed43c53cc46a359d347aea55a7f7471c9a0
ywtnb35 <watsyk35@outlook.jp>
2023-06-04 00:11:10 +0000

    size

--- app/Http/Controllers/IslandController.php
+++ app/Http/Controllers/IslandController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Photo;
+use Intervention\Image\Facades\Image;
 
 class IslandController extends Controller
 {
     //島個別ページ
     public function index(Request $request)
     {
+        $photos = Photo::all();
+        
+        foreach($photos as $photo){
+        $image_path = 'public/img/'.$photo->island_img;
+        $image = Image::make($image_path);
+        $width = 422;
+        $height = 500;
+        $image->resize($width, $height);
+        $savePath = 'public/img/resized_' . $photo->island_img;
+        $image->save(storage_path('app/' . $savePath));
+        $photo->resized_img = $savePath;
+        $photo->save();
+        
         $island_name = $request->name;
         $genre = $request->genre;
         $island_img = $request->name . ".jpg";
@@ -21,14 +35,15 @@ class IslandController extends Controller
         }
         
         return view('island.top', ['island_name'=> $island_name, 'island_img'=> $island_img, 'photos'=> $photos]);
+        }
+    
     }
     
-    public function create(Request $request)
-    {
-        if(!auth()->check()){
-            return redirect()->route('register');
-        }
-        return view('islnadphoto.create');
-    }
+    // public function create(Request $request)
+    // {
+    //     if(!auth()->check()){
+    //         return redirect()->route('register');
+    //     }
+    //     return view('islnadphoto.create');
+    // }
 }
-
--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Photo;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 
 class PhotoController extends Controller
