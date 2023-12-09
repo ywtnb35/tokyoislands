3677d498185dbadf7ff56fe4bb9e70c5413ce8aa
ywtnb35 <watsyk35@outlook.jp>
2023-12-09 07:49:14 +0000

    いいね機能

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -22,11 +22,7 @@ class LikeController extends Controller
         $user_id = auth()->user()->id;
         $like = Like::where('photo_id', $photo_id)->where('user_id', $user_id)->first();
     
-        if ($like) {
-            // 既に「いいね」されていれば、それを削除
-            $like->delete();
-        } else {
-            // まだ「いいね」されていなければ、新しく追加
+        if (!$like) { //すでにいいねされていなければ新しく追加
             $like = new Like();
             $like->photo_id = $photo_id;
             $like->user_id = $user_id;
@@ -39,7 +35,8 @@ class LikeController extends Controller
     
     public function destroy($photo_id)
     {
-        $like = Like::where('photo_id',$photo_id)->whre('user_id',auth()->user()->id)->first();
+        $user_id = auth()->user()->id;
+        $like = Like::where('photo_id',$photo_id)->whre('user_id',$user_id)->first();
         if ($like){
             $like->delete();
         }
