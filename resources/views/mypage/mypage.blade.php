8d7b9c25b1465e2a34a0961f521eb604ac2244f6
ywtnb35 <watsyk35@outlook.jp>
2024-01-02 01:58:49 +0000

    画像サイズ

--- resources/views/mypage/mypage.blade.php
+++ resources/views/mypage/mypage.blade.php
@@ -24,24 +24,27 @@
     </div>
  
     <hr>
-    <div class="photolist">
-    @isset($photos)
-        @foreach($photos as $photo)
-            <div class="photo-item">
-                <a href="{{ route('mypage.detail',['id'=>$photo->id]) }}" ><img src="{{ secure_asset('storage/img/' . $photo->island_image) }}" alt=""></a>
-                <p class="text_size" style="white-space:pre-wrap;">{{ $photo->text }}</p>
-                <!--いいね表示-->
-            <div class="like-button">
-                @if ($photo->is_liked_by_auth_user() == 1) 
-                    <button class="liked like" data-photo-id="{{ $photo->id }}">いいね</button>
-                @else 
-                    <button class="like" data-photo-id="{{ $photo->id }}">いいね</button>
-                @endif
-                    <span id="like-count-{{ $photo->id }}">{{ $photo->likes()->count() }}件</span>
-            </div> 
+    
+    <div class="photo-container">
+            <div class="island-detail">
+                @isset($photos)
+                    @foreach($photos as $photo)
+                    
+                    <div class="photo-item">
+                        <a href="{{ route('photo.show',['id'=>$photo->id]) }}" ><img src="{{ secure_asset('storage/img/' . $photo->island_image) }}" alt=""></a>
+                    <div class="like-button">
+                        @if ($photo->is_liked_by_auth_user() == 1) 
+                            <button class="liked like" data-photo-id="{{ $photo->id }}">いいね</button>
+                        @else 
+                            <button class="like" data-photo-id="{{ $photo->id }}">いいね</button>
+                        @endif
+                            <span id="like-count-{{ $photo->id }}">{{ $photo->likes()->count() }}件</span>
+                    </div> 
+                    </div>
+                    @endforeach
+                @endisset
             </div>
-        @endforeach
-    @endisset
-    </div>
+        </div>
+
 </div>
 @endsection
